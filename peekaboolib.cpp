fb002294dd3951020be947b10ef80f5c97520b20
Julian Andres Guarin <julian.guarin@fitech.com.co>
2016-09-09 18:58:49 -0500

    Debug Message type stripped from message

--- peekaboolib.cpp
+++ peekaboolib.cpp
@@ -11,28 +11,28 @@ void Peekaboolib::defaultCallBack(QtMsgType type, const QMessageLogContext &cont
     QString aColorName;
        switch (type) {
        case QtDebugMsg:
-           aString = QString("Debug [%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
-           fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+           aString = QString("[%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
+           fprintf(stderr, "%s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
            aColor = Qt::green;
            break;
        case QtInfoMsg:
-           aString = QString("Info [%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
-           fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+           aString = QString("[%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
+           fprintf(stderr, "%s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
            aColor = Qt::cyan;
            break;
        case QtWarningMsg:
-           aString = QString("Warning [%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
-           fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+           aString = QString("[%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
+           fprintf(stderr, "%s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
            aColor = Qt::yellow;
            break;
        case QtCriticalMsg:
-           aString = QString("Critical [%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
-           fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+           aString = QString("[%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
+           fprintf(stderr, "%s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
            aColor = Qt::red;
            break;
        case QtFatalMsg:
-           aString = QString("Fatal [%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
-           fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+           aString = QString("[%2:%3, %4]: %1\n").arg(localMsg.constData()).arg(context.file).arg(QString::number(context.line)).arg(context.function);
+           fprintf(stderr, "%s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
            aColor = Qt::magenta;
            abort();
        }
